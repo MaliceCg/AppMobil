70daaba4620f37efbbbe15b35440f599649e1e7f
Marine Cantaloube Girona <88738724+MaliceCg@users.noreply.github.com>
2024-03-12 16:27:06 +0100

    Initial Commit

---
+++ Shared/AppMobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppMobileApp.swift
+//  Shared
+//
+//  Created by Marine Cantaloube girona on 12/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppMobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Marine Cantaloube girona on 12/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
