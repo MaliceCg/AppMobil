cc2994ddca16723d38e094965926ad5444fbe2e8
Marine Cantaloube Girona <88738724+MaliceCg@users.noreply.github.com>
2024-03-13 12:00:33 +0100

    ContentView premier page

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -1,14 +1,47 @@
+
 // ContentView.swift
 import SwiftUI
 
-
 struct ContentView: View {
-    // Ajoutez cette ligne pour initialiser LoginViewModel
-    @ObservedObject var viewModel = RegisterViewModel()
+    @ObservedObject var RegisViewModel = RegisterViewModel()
+    @ObservedObject var LogViewModel = LoginViewModel()
 
     var body: some View {
-        // Modifiez cette ligne pour passer viewModel à LoginView
-        RegisterView(viewModel: viewModel)
+        VStack(spacing: 20) {
+            Image("logo")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 200)
+            Text("Bienvenue au Festival du Jeu de Montpellier")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(.blue)
+            Text("Pour pouvoir participer à l'organisation et t'inscrire à des postes, crée toi un compte.")
+                .font(.title3)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+                .padding(.horizontal, 20)
+            NavigationLink(destination: RegisterView(viewModel: RegisViewModel)) {
+                Text("S'inscrire")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+            }
+            NavigationLink(destination: LoginView(viewModel: LogViewModel)) {
+            
+                Text("Se connecter")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                    .frame(maxWidth: .infinity)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
     }
 }
 
